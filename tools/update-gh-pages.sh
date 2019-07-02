36b8e951d5894c34c9cd98b157d740ea9725860c
Bengt Martensson <barf@bengt-martensson.de>
2019-07-02 11:39:29 +0200

    API doc improvements. Resolves #8.

---
+++ tools/update-gh-pages.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+if [ ! -d target ] ; then
+    mkdir target
+fi
+
+doxygen
+
+WORKDIR=gh-pages
+ORIGINURL=`git remote get-url origin`
+
+rm -rf ${WORKDIR}
+git clone --depth 1 -b gh-pages ${ORIGINURL} ${WORKDIR}
+cd ${WORKDIR}
+#rm -f *
+cp -rf ../target/api-doc/* .
+git add *
+git commit -a -m "Update of API documentation"
+echo Now perform \"git push\" from ${WORKDIR}
